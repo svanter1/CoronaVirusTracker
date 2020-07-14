a7c3309c26b190ceb87db7cc5ce7c48cd6577f74
shashi <svanter1@asu.edu>
2020-07-13 23:40:05 -0700

    changed code

--- src/main/java/com/shashi/covidtracker/CovidtrackerApplication.java
+++ src/main/java/com/shashi/covidtracker/CovidtrackerApplication.java
@@ -2,15 +2,10 @@ package com.shashi.covidtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CovidtrackerApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CovidtrackerApplication.class);
-	}
+public class CovidtrackerApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidtrackerApplication.class, args);
